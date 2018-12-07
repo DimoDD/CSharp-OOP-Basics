1e6aff052d61ce6feb763d74ecf8c9817ffa4224
dimodd1 <dimodd1@yahoo.com>
2018-12-07 18:59:24 +0200

    Аdd vehicle problem and its solution from Polymorphism

---
+++ Polymorphism/Vehicles/Vehicles/Vehicle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vehicles.Vehicles.Contracts;
+
+namespace Vehicles.Vehicles
+{
+    public abstract class Vehicle : IVehicle
+    {
+        private double fuelQuantity;
+        private double fuelConsumtion;
+
+        protected Vehicle(double fuelQuantity, double fuelConsumption)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            //to do add validation
+            get { return this.fuelConsumtion; }
+            set { this.fuelConsumtion = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            double neededFuel = distance * this.FuelConsumption;
+            if (this.FuelQuantity < neededFuel)
+            {
+                throw new ArgumentException($"{this.GetType().Name} needs refueling");                
+            }
+            else
+            {
+                this.FuelQuantity -= neededFuel;
+                Console.WriteLine($"{this.GetType().Name} travelled {distance} km");
+            }
+        }
+
+        public void Refuel(double fuel)
+        {
+            if (this is Truck)
+            {
+                fuel *= 0.95;
+            }
+
+            this.fuelQuantity += fuel;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name}: {this.fuelQuantity:F2}";
+        }
+    }
+}
